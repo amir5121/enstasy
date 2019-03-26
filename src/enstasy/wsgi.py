903858d829c7fc33bd7ad1c0b4800e40e47accc5
Amir Hosein <amirhoseinheshmati@gmail.com>
2019-03-26 14:32:58 +0430

    renamed project

--- src/enstasy/wsgi.py
+++ src/enstasy/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for enstay project.
+WSGI config for enstasy project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enstay.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enstasy.settings')
 
 application = get_wsgi_application()
